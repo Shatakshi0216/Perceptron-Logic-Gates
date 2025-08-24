d0e59479be8e634318f895c10ea2088496242534
Shatakshi Tiwari <shatakshitiwari021@gmail.com>
2025-08-24 12:12:37 +0530

    files

---
+++ gates.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x = np.array([[0,0],[0,1],[1,0],[1,1]])
+#print(x)
+
+#Output
+AND = np.array([0,0,0,1])
+OR = np.array([0,1,1,1])
+NAND = np.array([1,1,1,0])
+XOR = np.array([0,1,1,0])   
